80ec86d2c1b79c46ed425f0b95fa59be535e20f2
Francis <francisgbohunmi@gmail.com>
2024-06-02 16:57:52 +0100

    Fixed an Error that troubled for days, I forgot to Add "use client" in one of my Components and it was rendering as a Server side code.

--- e-commerce-webapp/src/components/Hero.js
+++ e-commerce-webapp/src/components/Hero.js
@@ -1,58 +1,68 @@
+"use client";
+
 import React from 'react';
 import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+const Slide = ({ img, title, mainTitle, price }) => (
+  <div>
+    <img src={img} alt={title} />
+    <h3>{title}</h3>
+    <h2>{mainTitle}</h2>
+    <p>{price}</p>
+  </div>
+);
 
 const Hero = () => {
-    let settings = {
-        dots: true,
-        infinite: true,
-        autoplay: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        pauseOnHover: false,
-    };
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    pauseOnHover: false,
+  };
 
-    const slideData = [
-        {
-            id: 0,
-            img: "/banner-1.jpg",
-            title: "Trending Item",
-            mainTitle: "WOMEN'S LATEST FASHION SALE",
-            price: "$30",
-        },
-        {
-            id: 1,
-            img: "/banner-2.jpg",
-            title: "Trending Accessories",
-            mainTitle: "MODERN SUNGLASSES",
-            price: "$15",
-        },
-        {
-            id: 2,
-            img: "/banner-3.jpg",
-            title: "Sale Offer",
-            mainTitle: "NEW FASHION SUMMER SALE",
-            price: "$29",
-        },
-    ];
+  const slideData = [
+    {
+      id: 0,
+      img: "/banner-1.jpg",
+      title: "Trending Item",
+      mainTitle: "WOMEN'S LATEST FASHION SALE",
+      price: "$30",
+    },
+    {
+      id: 1,
+      img: "/banner-2.jpg",
+      title: "Trending Accessories",
+      mainTitle: "MODERN SUNGLASSES",
+      price: "$15",
+    },
+    {
+      id: 2,
+      img: "/banner-3.jpg",
+      title: "Sale Offer",
+      mainTitle: "NEW FASHION SUMMER SALE",
+      price: "$29",
+    },
+  ];
 
-    return (
-        <div>
-            <div>
-                <slider {...settings}> 
-                    {slideData.map((item)=> {
-                        return <Slide
-                        key = {item.id}
-                        img = {item.img}
-                        title = {item.title}
-                        mainTitle = {item.mainTitle}
-                        price = {item.price}
-
-                        />
-                    })}
-                </slider>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <Slider {...settings}>
+        {slideData.map((item) => (
+          <Slide
+            key={item.id}
+            img={item.img}
+            title={item.title}
+            mainTitle={item.mainTitle}
+            price={item.price}
+          />
+        ))}
+      </Slider>
+    </div>
+  );
 };
 
 export default Hero;
